391a6c8b6f90fa071d14ada4efb14c14ba25d9ac
Xuan6961 <git@github.com>
2019-08-07 16:45:08 +1000

    add product detail

--- classes/Product.php
+++ classes/Product.php
@@ -5,15 +5,15 @@ class Product extends Database{
         parent::__construct();
     }
     public function getProducts(){
-        $query = "SELECT
-        product.product_id,
+        $query = "
+        SELECT
+        @product_id := product.product_id as product_id,
         product.name,
         product.description,
         product.price,
-        image.image_file_name AS image
+        @image_id := ( SELECT image_id FROM product_image WHERE product_id = @product_id LIMIT 1) as image_id,
+        ( SELECT image_file_name FROM image WHERE image_id = @image_id ) as image
         FROM product
-        INNER JOIN product_image ON product.product_id = product_image.product_id
-        INNER JOIN image ON product_image.image_id = image.image_id
         ";
 
         $statement = $this -> connection -> prepare($query);
