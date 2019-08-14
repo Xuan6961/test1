04898f537b867e11713c8be200bbeaa1c86b6f5a
Xuan6961 <git@github.com>
2019-08-14 16:33:55 +1000

    addNavigation

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 require('vendor/autoload.php');
 
+//test for navigation after auth
+ session_start();
+// $_SESSION['auth'] = true;
+session_destroy();
+
+use aitsydney\Navigation;
+
+$nav = new Navigation();
+$nav_items = $nav -> getNavigation();
+
 use aitsydney\Product;
 $products = new Product();
 $products_result = $products -> getProducts();
@@ -16,6 +26,7 @@ $template = $twig -> load('home.twig');
 
 //pass values to twig
 echo $template ->render([
+    'navigation' => $nav_items,
     'products' => $products_result,
     'title' => 'Hello shop'
 ]);
