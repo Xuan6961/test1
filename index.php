a44efdbcdc75aa36e369cd696b4179667b855653
Xuan6961 <git@github.com>
2019-08-21 14:49:58 +1000

    Add Category

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 require('vendor/autoload.php');
 
 //test for navigation after auth
- session_start();
+ //session_start();
 // $_SESSION['auth'] = true;
-session_destroy();
+//session_destroy();
 
 use aitsydney\Navigation;
 
@@ -14,6 +14,11 @@ $nav_items = $nav -> getNavigation();
 use aitsydney\Product;
 $products = new Product();
 $products_result = $products -> getProducts();
+
+use aitsydney\Category;
+
+$cat = new Category();
+$categories = $cat -> getCategories();
 
 //create twig loader
 $loader = new Twig_Loader_Filesystem('templates');
@@ -26,6 +31,7 @@ $template = $twig -> load('home.twig');
 
 //pass values to twig
 echo $template ->render([
+    'categories' => $categories,
     'navigation' => $nav_items,
     'products' => $products_result,
     'title' => 'Hello shop'
