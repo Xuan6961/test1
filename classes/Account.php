67623b941612bdb7c810edae59d91f7ccc27ffe9
Xuan6961 <git@github.com>
2019-08-28 16:43:38 +1000

    add register page

--- classes/Account.php
+++ classes/Account.php
@@ -11,11 +11,12 @@ class Account extends Database{
 
     public function register( $email, $password ){
         $query = "
-            INSERT INTO account (account_id, email, password, created)
-            VALUES (?,?,?,NOW() )
+            INSERT INTO account (account_id , email, password, created, accessed, updated)
+            VALUES ( UNHEX(?), ?, ?, NOW(), NOW(), NOW() )
         ";
 
         $register_errors = array();
+        $response = array();
 
         if( strlen($password) < 8 ){
             $register_errors['password'] ="minimum 8 charachers";
@@ -31,25 +32,33 @@ class Account extends Database{
             $account_id = $this -> createAccountId();
             try{
                 if( $statement = $this -> connection -> prepare( $query ) == false ){
-                    throw( new Exception('query error') );
+                    throw( new \Exception('query error') );
                 }
 
                 $statement -> bind_param('sss', $account_id, $email, $hash );
 
-                if($statement -> exeute() == false ){
-                    throw( new Exception('failed to execute') );
+                if( $statement -> exeute() == false ){
+                    throw( new \Exception('failed to execute') );
                 }
                 else{
                     //account is created in database
-                    
+                    $response['success'] = true;
                 }
             }
+            catch( Exception $exc ){
+                error_log( $exc ->getMessage() );
+            }
         }
-
+        else {
+            $response['errors'] = $register_errors;
+            $response['success'] = false;
+        }
+        return $response;
     }
     private function createAccountId(){
         //get random bytes
-        $bytes = openssl_random_pseudo_bytes(16);
+        $bytes = random_bytes(16);
+        //$bytes = random_bytes(16);
         //convert to hexadecimal
         $str = bin2hex( $bytes );
         return $str;
